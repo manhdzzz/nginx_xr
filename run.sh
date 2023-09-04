b753b3d7848d4f1a96486a5bd3851bb59705e2f7
manhdzzz <101183159+manhdzzz@users.noreply.github.com>
2023-09-04 09:59:46 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
 sudo apt update
 sudo apt upgrade
-sudo ufw allow 80/tcp
-sudo ufw allow 443/tcp
-sudo ufw allow 80
-sudo ufw allow 443
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
